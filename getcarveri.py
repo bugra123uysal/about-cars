b84dd5b64c48e1395668dd27665794c1cd761fac
buğra uysal <uysalbugra134@gmail.com>
2024-07-29 01:08:15 +0300

    araba markası  girip fiyatları ve moddelleri gözükür

--- getcarveri.py
+++ getcarveri.py
@@ -7,20 +7,46 @@ rr=tk.Tk()
 rr.geometry("1000x1000")
 
 def üüp():
-    urlm="https://tr.wikipedia.org/wiki/BMW"
+    qqo=ww.get()
+    urlm=f"https://www.arabam.com/ikinci-el?searchText={qqo}"
+
+
+    
 
 
     aal=requests.get(urlm)
     l=aal.content
     vv=BeautifulSoup(l,'html.parser')
-    tur=vv.find_all('p')
-    for nn in tur:
-         ll.config(text=nn.text)
-""" button """
-gg=tk.Button(rr, text="all", command=üüp )
-gg.place(x=100, y=100)
+    """ fiyat """
+    tur=vv.find_all('span', class_='db no-wrap listing-price' )
+    fiyat_list=[fiyat.text.strip() for fiyat in tur]
 
-""" label """
+    ll.config(text="\n".join(fiyat_list[:100]))
+    """ model """
+    mdol=vv.find_all('h4',  class_="crop-after crop-after-40 odd-ca40"  )  
+
+   
+    isim_list=[mm.text.strip() for mm in mdol]  
+
+    
+    ff.config(text="\n".join(isim_list[:100]))
+
+""" button """
+gg=tk.Button(rr, text="al", command=üüp )
+gg.place(x=30, y=1)
+
+""" label fiyat """
 ll=tk.Label(rr)
-ll.place(x=50,y=50)
+ll.place(x=50,y=210)
+""" label model ismi"""
+ff=tk.Label(rr,)
+ff.place(x=400, y=40)
+
+ww=tk.Entry(rr,)
+ww.place(x=50, y=10)
 rr.mainloop()
+
+
+
+""" arama verisi 
+ """
